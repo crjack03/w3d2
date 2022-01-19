6ccc9a0f35855056b80d5ba810fcd983a8b5cddd
Christopher Jackson <Chris2003jackson@gmail.com>
2022-01-18 21:33:25 -0500

    push

---
+++ app.js
@@ -0,0 +1,12 @@
+var myVar = 1;
+myVar ++;
+console.log();
+var myVar2 = 2;
+myVar2 --;
+console.log();
+var myVar3 = 3;
+myVar3 = 3 * 3;
+console.log();
+var myVar4 = 4;
+myVar4 = 4 / 2;
+console.log()
